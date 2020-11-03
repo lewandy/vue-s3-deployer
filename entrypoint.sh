5bbde865328a612dad6bef7ef35875d8bd0d6e1b
Lewandy Dilone <lewandydilone1@live.com>
2020-11-03 09:08:25 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-#Move to app rource code
+#Move to app source code
 cd $INPUT_APP_SOURCE_CODE
 
 #Install dependencies
-npm install 
+echo "Installing dependencies"
+npm install > /dev/null 2>&1
 
 #Build application
-npm run build
+echo "Building application"
+npm run build > /dev/null 2>&1
 
 #Sync files with amazon s3 bucket app
-aws --region $INPUT_AWS_DEFAULT_REGION s3 sync ./dist s3://$INPUT_AWS_BUCKET_NAME --delete
+aws --region $INPUT_AWS_DEFAULT_REGION s3 sync ./dist s3://$INPUT_AWS_BUCKET_NAME --no-progress --delete
